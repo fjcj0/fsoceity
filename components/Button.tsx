32ea6ed577e72e9bd6b2f1ddae086a499de2bdc4
omar <omarqais225@gmail.com>
2026-01-02 22:13:16 +0200

    add some styles to global css

--- components/Button.tsx
+++ components/Button.tsx
@@ -14,30 +14,6 @@ export default function Button({
 }) {
     return (
         <>
-            <style>{`
-                @keyframes rotate {
-                    100% {
-                        transform: rotate(1turn);
-                    }
-                }
-
-                .rainbow::before {
-                    content: '';
-                    position: absolute;
-                    z-index: -2;
-                    left: -50%;
-                    top: -50%;
-                    width: 200%;
-                    height: 200%;
-                    background-position: 100% 50%;
-                    background-repeat: no-repeat;
-                    background-size: 50% 30%;
-                    filter: blur(6px);
-                    background-image: linear-gradient(#FFF);
-                    animation: rotate 4s linear infinite;
-                }
-            `}</style>
-
             <div className="w-full rainbow relative z-0 bg-white/10 overflow-hidden p-0.5 flex items-center justify-center rounded-full transition duration-300 active:scale-100">
                 <button
                     type="submit"
