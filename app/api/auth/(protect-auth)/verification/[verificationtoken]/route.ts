622a8011202e3f734605aeb554e32d68ab5fd5de
omar <omarqais225@gmail.com>
2026-01-03 19:04:00 +0200

    fix params

--- app/api/auth/(protect-auth)/verification/[verificationtoken]/route.ts
+++ app/api/auth/(protect-auth)/verification/[verificationtoken]/route.ts
@@ -1,35 +1,41 @@
 import { prisma } from "@/app/lib/prisma";
 import { blockAuthenticatedUser } from "@/middleware/user.middleware";
 import { NextRequest, NextResponse } from "next/server";
-export async function GET(request: NextRequest, { params }: { params: { verificationtoken: string } }) {
+export async function GET(
+    request: NextRequest,
+    { params }: { params: { verificationtoken: string } }
+) {
     try {
         const block = await blockAuthenticatedUser(request);
         if (block) return block;
-        const verificationToken: string = params.verificationtoken;
-        const isUserFound: boolean = await prisma.user.findFirst({
+        const { verificationtoken } = params;
+        const isUserFound = await prisma.user.findFirst({
             where: {
-                verificationToken,
+                verificationToken: verificationtoken,
                 verificationTokenExpiresAt: {
                     gt: new Date(),
                 },
-            }
-        }) ? true : false;
-        if (!isUserFound) return NextResponse.json({
-            success: false,
-            message: `The link expired`,
-        }, {
-            status: 400
-        });
-        return NextResponse.json({
-            success: true,
-            message: `The link is correct`,
-        }, {
-            status: 200
-        });
+            },
+        })
+            ? true
+            : false;
+        if (!isUserFound) {
+            return NextResponse.json(
+                { success: false, message: "The link expired" },
+                { status: 400 }
+            );
+        }
+        return NextResponse.json(
+            { success: true, message: "The link is correct" },
+            { status: 200 }
+        );
     } catch (error: unknown) {
-        return NextResponse.json({
-            error: `Fatal Error: ${error instanceof Error ? error.message : error}`,
-            success: false,
-        }, { status: 500 });
+        return NextResponse.json(
+            {
+                success: false,
+                error: `Fatal Error: ${error instanceof Error ? error.message : error}`,
+            },
+            { status: 500 }
+        );
     }
 }
--- app/api/auth/(protect-reset-password)/reset-password/[resettoken]/route.ts
+++ app/api/auth/(protect-reset-password)/reset-password/[resettoken]/route.ts
@@ -1,32 +1,38 @@
 import { prisma } from "@/app/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
-export default async function GET(request: NextRequest, { params }: { params: { resettoken: string } }) {
+export async function GET(
+    request: NextRequest,
+    { params }: { params: { resettoken: string } }
+) {
     try {
-        const resetToken: string = params.resettoken;
-        const isUserFound: boolean = await prisma.user.findFirst({
+        const { resettoken } = params;
+        const isUserFound = await prisma.user.findFirst({
             where: {
-                resetToken,
+                resetToken: resettoken,
                 resetTokenExpiresAt: {
-                    gt: new Date()
-                }
-            }
-        }) ? true : false;
-        if (!isUserFound) return NextResponse.json({
-            success: false,
-            message: `The link expired`,
-        }, {
-            status: 400
-        });
-        return NextResponse.json({
-            success: true,
-            message: `The link is correct`,
-        }, {
-            status: 200
-        });
+                    gt: new Date(),
+                },
+            },
+        })
+            ? true
+            : false;
+        if (!isUserFound) {
+            return NextResponse.json(
+                { success: false, message: "The link expired" },
+                { status: 400 }
+            );
+        }
+        return NextResponse.json(
+            { success: true, message: "The link is correct" },
+            { status: 200 }
+        );
     } catch (error: unknown) {
-        return NextResponse.json({
-            success: false,
-            error: `Fatal Error: ${error instanceof Error ? error.message : error}`
-        }, { status: 500 });
+        return NextResponse.json(
+            {
+                success: false,
+                error: `Fatal Error: ${error instanceof Error ? error.message : error}`,
+            },
+            { status: 500 }
+        );
     }
 }
