1dcea9535c59730f4d1a0c70d9947e2616691481
omar <omarqais225@gmail.com>
2026-01-05 00:04:50 +0200

    fix all auth issues

--- middleware/user.middleware.ts
+++ middleware/user.middleware.ts
@@ -1,7 +1,6 @@
 import { jwtVerify } from 'jose';
 import { NextRequest, NextResponse } from 'next/server';
 import { UserType } from '@/global';
-import { prisma } from '@/app/lib/prisma';
 const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET!);
 export async function blockAuthenticatedUser(request: NextRequest) {
     const token = request.cookies.get('token')?.value;
@@ -9,13 +8,17 @@ export async function blockAuthenticatedUser(request: NextRequest) {
     try {
         const { payload } = await jwtVerify(token, JWT_SECRET);
         const user = payload.user as UserType;
-        const isFound: boolean = await prisma.user.findUnique({
-            where: {
-                id: user?.id,
-            }
-        }) ? true : false;
-        if (isFound) return NextResponse.json({ success: false, error: 'You are already logged in' },
-            { status: 403 });
+        if (user) {
+            return NextResponse.json(
+                {
+                    success: false,
+                    error: 'You are already logged in'
+                },
+                {
+                    status: 403
+                }
+            );
+        }
         return null;
     } catch (error: unknown) {
         throw new Error(`Fatal Error: ${error instanceof Error ? error.message : error}`)
