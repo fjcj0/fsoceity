df0248a10a91f2aee5b57b66da0c2ecbb03a158a
omar <omarqais225@gmail.com>
2026-01-06 01:26:53 +0200

    add delete post to backend api

---
+++ app/api/auth/(protect-user)/delete-post/[id]/route.ts
@@ -0,0 +1,25 @@
+import { prisma } from "@/app/lib/prisma";
+import { authMiddleware } from "@/middleware/auth.middleware";
+import { NextRequest, NextResponse } from "next/server";
+export async function DELETE(request: NextRequest, context: { params: Promise<{ id: string }> }) {
+    try {
+        const { id } = await context.params;
+        const { error, user } = await authMiddleware(request);
+        if (error) return error;
+        await prisma.post.delete({
+            where: {
+                id,
+                authorId: user.id
+            }
+        });
+        return NextResponse.json({
+            message: 'Delete has done successfully',
+            succcess: true
+        }, { status: 200 });
+    } catch (error: unknown) {
+        return NextResponse.json({
+            success: false,
+            error: `Fatal Error: ${error instanceof Error ? error.message : error}`
+        }, { status: 500 });
+    }
+}
