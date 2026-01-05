0cdba820a511687501615afe5f2caa97817af07c
omar <omarqais225@gmail.com>
2026-01-05 18:41:35 +0200

    add notfication bell

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,6 +12,12 @@ const Header = () => {
                 <Image src={'/fsoceity.png'} width={60} height={60} alt="icon" />
             </div>
             <div className="flex items-center justify-center gap-3">
+                <button type="button" className="flex items-center mr-3 justify-center relative cursor-pointer">
+                    <Image src={'/bell.png'} width={30} height={30} alt="bell icon" />
+                    <div className="flex items-center justify-center absolute bg-red-500 w-3 h-3 rounded-full right-0.5  -top-1 bottom-0">
+                        <p className="" style={{ fontSize: 7 }}>0</p>
+                    </div>
+                </button>
                 <Button
                     title="logout"
                     onClick={logout}
@@ -20,6 +26,7 @@ const Header = () => {
                 />
                 <button type="button" onClick={toggleHeaderSlide} className="cursor-pointer active:opacity-50 active:scale-75 duration-300 transition-all md:hidden">
                     <List />
+
                 </button>
             </div>
         </div>
