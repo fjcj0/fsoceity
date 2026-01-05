ac5063807fab0f0194ce47f179ae74ea4d47e078
omar <omarqais225@gmail.com>
2026-01-05 20:57:31 +0200

    create backend to post content and creat functionality to get all users posts-bookmarks-likes

--- app/api/auth/(protect-user)/post-content/route.ts
+++ app/api/auth/(protect-user)/post-content/route.ts
@@ -38,3 +38,29 @@ export async function POST(request: NextRequest) {
         }, { status: 500 });
     }
 }
+export async function GET(request: NextRequest) {
+    try {
+        const { user, error } = await authMiddleware(request);
+        if (error) return error;
+        const user_data = await prisma.user.findUnique({
+            where: {
+                id: user.id,
+            }, select: {
+                posts: true,
+                bookmarks: true,
+                likes: true
+            }
+        });
+        return NextResponse.json({
+            user_posts: user_data?.posts,
+            user_bookmarks: user_data?.bookmarks,
+            user_likes: user_data?.likes,
+            success: true,
+        }, { status: 200 });
+    } catch (error: unknown) {
+        return NextResponse.json({
+            error: `Fatal Error: ${error instanceof Error ? error.message : error}`,
+            success: false
+        }, { status: 500 });
+    }
+}
