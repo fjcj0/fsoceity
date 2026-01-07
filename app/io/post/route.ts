355692e01023de317d8a91e9e5e328573c2b2938
omar <omarqais225@gmail.com>
2026-01-07 19:44:06 +0200

    create routes for socket

---
+++ app/io/post/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest } from "next/server";
+export async function GET(request: NextRequest) {
+    try {
+
+    } catch (error: unknown) {
+        console.log(`Error socket not connected...`);
+    }
+}
