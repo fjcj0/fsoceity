f81583732c5c2d3241a76c7cebeb07d6a477c701
omar <omarqais225@gmail.com>
2026-01-08 01:42:40 +0200

    an errors with sockets.io

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -1,15 +1,12 @@
-import { ChatProvider } from '@/context/ChatContext';
 import ChatLayoutClient from '@/layouts/ChatLayoutClient';
 import { ReactNode } from 'react';
 const ChatLayout = ({ children }: { children: ReactNode }) => {
     return (
-        <ChatProvider>
-            <div className='w-screen min-h-screen bg-[#161616]'>
-                <ChatLayoutClient>
-                    {children}
-                </ChatLayoutClient>
-            </div>
-        </ChatProvider>
+        <div className='w-screen min-h-screen bg-[#161616]'>
+            <ChatLayoutClient>
+                {children}
+            </ChatLayoutClient>
+        </div>
     );
 };
 export default ChatLayout;
