4784982d31694af207baf1c24a40dcc3501a0dfa
omar <omarqais225@gmail.com>
2026-01-07 12:11:50 +0200

    add socket to package

--- layouts/ChatLayoutClient.tsx
+++ layouts/ChatLayoutClient.tsx
@@ -1,12 +1,14 @@
 "use client";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import Slider from "@/components/Slider";
 import { links, links_chat } from "@/constants/data";
 import Header from "@/components/Header";
 import SliderMobile from "@/components/SliderMobile";
 import useHeaderStore from "@/store/HeaderStore";
+import useAuthStore from "@/store/AuthStore";
 const ChatLayoutClient = ({ children }: { children: ReactNode }) => {
     const { isHeaderSlideOpen } = useHeaderStore();
+    const { user } = useAuthStore();
     return (
         <main className="h-screen w-screen flex bg-[#0f0f0f] text-white">
             <div className={`fixed w-[15rem] ${!isHeaderSlideOpen && ' translate-x-72'} p-3 right-0 min-h-screen z-50 bg-[#242424] md:hidden duration-400 transition-all ease-in-out`}>
