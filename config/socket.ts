61bc65299f0883a4a2103e1bee1175805f389b79
omar <omarqais225@gmail.com>
2026-01-12 18:29:57 +0200

    make env run on production

--- config/socket.ts
+++ config/socket.ts
@@ -2,7 +2,7 @@ import { io, Socket } from "socket.io-client";
 let socket: Socket | null = null;
 export function getSocket(): Socket {
     if (!socket) {
-        socket = io(process.env.SERVER!, {
+        socket = io(process.env.NEXT_PUBLIC_SERVER, {
             withCredentials: true,
             autoConnect: false,
         });
