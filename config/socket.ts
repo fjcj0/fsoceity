ccd5479317b0ca7e41653650b86918496051b574
omar <omarqais225@gmail.com>
2026-01-12 18:50:10 +0200

    remove transporter

--- config/socket.ts
+++ config/socket.ts
@@ -2,10 +2,9 @@ import { io, Socket } from "socket.io-client";
 let socket: Socket | null = null;
 export function getSocket(): Socket {
     if (!socket) {
-        socket = io(process.env.NEXT_PUBLIC_SERVER, {
+        socket = io(process.env.NEXT_PUBLIC_SERVER!, {
             withCredentials: true,
             autoConnect: false,
-            transports: ["websocket"]
         });
         socket.connect();
     }
