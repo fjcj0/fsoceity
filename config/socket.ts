cc5dcfb977e4026c4807e4427c1b04d509a5d68e
omar <omarqais225@gmail.com>
2026-01-12 19:03:02 +0200

    force websocket

--- config/socket.ts
+++ config/socket.ts
@@ -3,6 +3,7 @@ let socket: Socket | null = null;
 export function getSocket(): Socket {
     if (!socket) {
         socket = io(process.env.NEXT_PUBLIC_SERVER!, {
+            transports: ["websocket"],
             withCredentials: true,
             autoConnect: false,
         });
